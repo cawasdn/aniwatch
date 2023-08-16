5d19663d86f6e37f44ead6e7db365802f72c3a0d
Daniel <cawasdn@gmail.com>
2023-08-16 19:43:47 +0800

    Added card component

--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,8 +1,11 @@
+import Card from './Card'
+
 const Results = ({ results }) => {
   return (
-    <div>
-      {results.map((results) => (
-        <div key={results.id}>{results.original_title}</div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
+      {results.map((result) => (
+        <Card key={result.id} result={result} />
+        // <div key={results.id}>{results.original_title}</div>
       ))}
     </div>
   )
