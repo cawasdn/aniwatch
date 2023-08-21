0b6303043cd3b5e1c88501184a9adac0efca6abe
Daniel <cawasdn@gmail.com>
2023-08-21 18:44:19 +0800

    Added search functionality

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,7 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 const SearchBox = () => {
+  const [search, setSearch] = useState('')
+  const router = useRouter()
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!search) return
+    router.push(`/search/${search}`)
+  }
+
   return (
-    <form>
-      <input type='text' placeholder='Search keywords...' />
+    <form
+      onSubmit={handleSubmit}
+      className='flex max-w-6xl mx-auto justify-between items-center px-5'
+    >
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type='text'
+        placeholder='Search keywords...'
+        className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent'
+      />
+      <button
+        disabled={!search}
+        type='submit'
+        className='text-amber-600 disabled:text-gray-400 flex-1'
+      >
+        Search
+      </button>
     </form>
   )
 }
